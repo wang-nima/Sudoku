c9ecc1bb249c28850c0b02abc96772b42d5547f2
shawn <jak47042007@163.com>
2015-04-02 22:01:54 -0700

    change button image to text

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -6,56 +6,36 @@ USING_NS_CC;
 
 Scene* MainMenuScene::createScene()
 {
-    // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
     auto layer = MainMenuScene::create();
-
-    // add layer as a child to scene
     scene->addChild(layer);
-
-    // return the scene
     return scene;
 }
 
-// on "init" you need to initialize your instance
 bool MainMenuScene::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
+    if ( !LayerColor::initWithColor(Color4B::WHITE)) {
         return false;
     }
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
-    auto backGroundSprite = Sprite::create("Background.jpg");
-    backGroundSprite->setPosition( Point(visibleSize.width / 2 + origin.x,
-                                         visibleSize.height / 2 + origin.y));
     
-    this->addChild(backGroundSprite);
+    auto menu_item_1 = MenuItemFont::create("Play", CC_CALLBACK_1(MainMenuScene::GoToGameScene, this));
+    menu_item_1->setColor(Color3B::BLACK);
+    menu_item_1->setFontSize(100);
     
-    
-    auto playItem = MenuItemImage::create("Play Button.png", "Play Button Clicked.png",
-                                          CC_CALLBACK_1(MainMenuScene::GoToGameScene, this));
-
-    playItem->setPosition( Point(visibleSize.width / 2 + origin.x,
-                                visibleSize.height / 2 + origin.y));
-    auto menu = Menu::create(playItem, NULL);
-    menu->setPosition(Point::ZERO);
-    
+    auto menu = Menu::create(menu_item_1, nullptr);
+    menu->setPosition( Point(visibleSize.width / 2,
+                             visibleSize.height / 2) );
     this->addChild(menu);
     
+    auto title = Label::createWithTTF("Sudoku", "Naughty Cartoons.ttf", 156);
+    title->setPosition(visibleSize.width / 2, visibleSize.height / 2 + 300);
+    title->setColor(Color3B::BLACK);
+    this->addChild(title);
     
-    auto ttf1 = Label::createWithTTF("Sudoku", "Naughty Cartoons.ttf", 156);
-
-    
-    
-    ttf1->setPosition(visibleSize.width / 2, visibleSize.height / 2 + 300);
-    this->addChild(ttf1);
     
     return true;
 }
--- Classes/MainMenuScene.h
+++ Classes/MainMenuScene.h
@@ -3,19 +3,18 @@
 
 #include "cocos2d.h"
 
-class MainMenuScene: public cocos2d::Layer
+class MainMenuScene: public cocos2d::LayerColor
 {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-    
-    // implement the "static create()" method manually
     CREATE_FUNC(MainMenuScene);
 private:
     void GoToGameScene (cocos2d::Ref *sender);
+    void Play(Ref *pSender);
+    void Highscores(Ref *pSender);
+    void Settings(Ref *pSender);
+
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif 
